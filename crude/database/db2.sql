c2f33b9b85e36a42d5f1037f58767ca9037e9331
AngelIdk666 <83241924+AngelIdk666@users.noreply.github.com>
2024-10-31 07:35:54 -0600

    Update db2.sql
    DB Corregida

--- crude/database/db2.sql
+++ crude/database/db2.sql
@@ -8,22 +8,8 @@ CREATE TABLE `alumnos` (
   `nombre_alumno` varchar(255) DEFAULT NULL,
   `email_alumno` varchar(255) DEFAULT NULL,
   `curso_alumno` varchar(50) DEFAULT NULL,
-  `sexo_alumno` varchar(20) DEFAULT NULL,
-  `habla_ingles` varchar(20) DEFAULT NULL,
+  `tipo` varchar(20) DEFAULT NULL,
   `fecha_registro` timestamp NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- creating a table
-CREATE TABLE customer (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    address VARCHAR(100) NOT NULL,
-    phone VARCHAR(15)
-);
-
--- Show all tables
-SHOW TABLES;
-
--- To describe the table
-describe customer;
