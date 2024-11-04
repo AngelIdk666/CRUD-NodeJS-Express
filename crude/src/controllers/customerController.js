7dbb5d0cf8606142216a20a8223e052bc1a48989
AngelIdk666 <83241924+AngelIdk666@users.noreply.github.com>
2024-11-04 10:47:40 -0600

    Update customerController.js
    correccion cambios

--- crude/src/controllers/customerController.js
+++ crude/src/controllers/customerController.js
@@ -1,27 +1,79 @@
-const customerController = {
-    list: (req, res) => {
-        // Lógica para listar clientes
-        res.send('List of customers');
-    },
-    save: (req, res) => {
-        // Lógica para guardar un cliente
-        res.send('Customer saved');
-    },
-    delete: (req, res) => {
-        // Lógica para eliminar un cliente
-        const id = req.params.id;
-        res.send(`Customer with id ${id} deleted`);
-    },
-    edit: (req, res) => {
-        // Lógica para editar un cliente
-        const id = req.params.id;
-        res.send(`Editing customer with id ${id}`);
-    },
-    update: (req, res) => {
-        // Lógica para actualizar un cliente
-        const id = req.params.id;
-        res.send(`Customer with id ${id} updated`);
-    }
+const res = require("express/lib/response");
+
+const controller = {};
+
+controller.list = (req, res) => {
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM alumnos', (err, alumnos) => { // Cambié 'customers' a 'alumnos'
+            if (err) {
+                return res.json(err);
+            }
+
+            res.render('customers', { // Mantiene la vista de 'customers'
+                data: alumnos
+            });
+        });
+    });
 };
 
-module.exports = customerController;
+controller.save = (req, res) => {
+    const data = req.body;
+
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO alumnos set ?', [data], (err, alumno) => { // Cambié 'customer' a 'alumnos'
+            if (err) {
+                return res.json(err);
+            }
+
+            res.redirect('/');
+        });
+    });
+};
+
+controller.delete = (req, res) => {
+    const { id } = req.params;
+
+    req.getConnection((err, conn) => {
+        conn.query('DELETE FROM alumnos WHERE id = ?', [id], (err, rows) => { // Cambié 'customer' a 'alumnos'
+            if (err) {
+                return res.json(err);
+            }
+
+            res.redirect('/');
+        });
+    });
+};
+
+controller.edit = (req, res) => {
+    const { id } = req.params;
+
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM alumnos WHERE id = ?', [id], (err, alumno) => { // Cambié 'customer' a 'alumnos'
+            if (err) {
+                return res.json(err);
+            }
+
+            res.render('customer_edit', { // Mantiene la vista de 'customer_edit'
+                data: alumno[0]
+            });
+        });
+    });
+};
+
+controller.update = (req, res) => {
+    const { id } = req.params;
+    const newAlumno = req.body; // Cambié 'newCustomer' a 'newAlumno'
+    
+    req.getConnection((err, conn) => {
+        conn.query('UPDATE alumnos set ? WHERE id = ?', [newAlumno, id], (err, rows) => { // Cambié 'customer' a 'alumnos'
+            if (err) {
+                return res.json(err);
+            }
+            
+            res.redirect('/');
+        });
+    });
+};
+
+module.exports = controller;
+s
